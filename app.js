92d60fc9002e9279d2c3331fd29b047351a94374
CherutoMercy <m.thirty100@gmail.com>
2016-07-08 11:17:20 +0300

    display history of activities done

--- app.js
+++ app.js
@@ -8,18 +8,17 @@ var server=http.createServer(app);
 //initialize required modules for the app
 var bodyParser=require('body-parser');
 var morgan=require('morgan');
-//var mongoose=require('mongoose');
+var mongoose=require('mongoose');
 var mongodb=require('mongodb');
-
 var path=require('path')
 var bodyParser   = require('body-parser');
 var session=require('express-session');
 var methodOverride = require('method-override');  
 //var MongoClient = mongodb.MongoClient;
-var Activity=require('./routes/activity');
+var Activity=require('./models/activity');
 //connect to mongo database
-var url = 'mongodb://localhost:27017/math';
-
+var configDB = require('./config/database.js');
+mongoose.connect(configDB.url);
 //use required modules
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'views')));
@@ -28,16 +27,14 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride());
 app.set('view engine', 'html');
 
-var mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost/math');
 
 // Page.
-app.get('/', function(req, res){
+app.get('/calc', function(req, res){
     res.sendFile('calc.html', {'root' : 'views'});
 });
 
-//Return all the activities from the collection.
-app.get('/activity', function(req, res){
+//send activity to frontend
+app.get('/api/activity', function(req, res){
   Activity.find({}, function(err, activity){
     if(err)
       res.send(err)
@@ -45,6 +42,31 @@ app.get('/activity', function(req, res){
  });
 });
 
+
+ 
+
+
+       
+ //insert values into mongo db
+    app.post('/insert', function(req, res){
+        Activity.create({
+             num1 : req.body.num1,
+             num2 : req.body.num2,
+             rest : req.body.rest,
+             activity:req.body.activity
+            },
+            function(err, activity){
+           if(err)
+              res.send(err)
+
+           Activity.find({}, function(err, activity){
+              if(err)
+                  res.send(err);
+
+              res.redirect('calc.html');
+           });
+        });
+    });
 
 app.listen(port);
 console.log('Server running on localhost: port ' + port);
---
+++ models/activity.js
@@ -0,0 +1,15 @@
+//Schema for activity
+var mongoose=require('mongoose');
+//var Schema=mongoose.Schema;
+//Create a Schema model to hold your activity data.
+//var timestamps = require('mongoose-timestamp');
+var activitySchema = mongoose.Schema({
+    num1:Number,
+    num2:Number,
+    rest:Number,
+    activity:String,
+    time : { type : Date, default: Date.now }
+   
+});
+ module.exports =mongoose.model('Activity', activitySchema);
+    
